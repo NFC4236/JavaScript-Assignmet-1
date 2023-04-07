7627b7c694d996d89ba97a6eb0bd05c16c91b028
AMARA KHAN <96923038+NFC4236@users.noreply.github.com>
2023-04-07 18:08:44 +0500

    Update Assignment1Js.js

--- Assignment1Js.js
+++ Assignment1Js.js
@@ -10,6 +10,31 @@ if (Number(num1)>Number(num2)) {
 else  {
     alert("Second Number is Large :"+Number(num2));
 }
+//******************3rd Program*************************
+
+let value1 =+prompt("Enter No 1st ");
+let value2 =+prompt("Enter No 2nd ");
+let value3 =+prompt("Enter No 3rd ");
+let value4 =+prompt("Enter No 4th ");
+let value5 =+prompt("Enter No 5th ");
+
+if (value1>value2 && value1>value3 && value1>value4 && value1>value5  ) {
+           console.log("Large No is 1st :"+value1);
+} 
+else if (value2<value1 && value2<value3 && value2<value4 && value2<value5 ) {
+    console.log("Large No is 2nd :"+value2);
+} 
+else if (value3>value1 && value3>value2 && value3>value4 && value3>value5 ) {
+    console.log("Large No is 3rd :"+value3);
+} 
+else if (value4>value1 && value4>value2 && value4>value3 && value4>value5  ) {
+    console.log("Large No is 4th :"+value4);
+} 
+
+else if (value5>value1 && value5>value2 && value5>value3 && value5>value4 ) {
+    console.log("Large No is 5th :"+value5);
+} 
+
 
 //********************Four Program****************************
 
